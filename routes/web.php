05d0681a86475cb1ccd4212026ed9110548b3c35
Michael Eisenbraun <eisenbm@algonquincollege.com>
2022-02-27 10:08:17 -0500

    Creates starter file for part 2

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $title = "Notes";
+    return view('notes', ['title' => $title]);
+});
+
+Route::get('/notes/{note}', function ($id) {
+    $title = "Note {$id}";
+    $text = "This is note {$id}";
+    return view('note', ['title' => $title, 'text' => $text]);
 });
