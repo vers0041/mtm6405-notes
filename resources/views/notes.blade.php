f6d570c0f514ad2cf4ca9adc433ed15f1abfbe10
Michael Eisenbraun <eisenbm@algonquincollege.com>
2022-02-27 14:16:26 -0500

    Updates the notes blade templete

--- resources/views/notes.blade.php
+++ resources/views/notes.blade.php
@@ -1,12 +1,18 @@
 <x-layout>
   <x-slot name="title">{{ $title }}</x-slot>
   <h1 class="display-4 my-5">{{ $title }}</h1>
-  
+  <div class="row">
+    <div class="col">
+      <form class="form mb-3">
+        <input type="text" class="form-control" name="search" placeholder="Search">
+      </form>
+    </div>
+  </div>
   <div class="row">
   @foreach ($notes as $note)
     <div class="col-12 col-md-6 col-lg-4">
       <a href="/notes/{{ $note['id'] }}" class="text-decoration-none text-body">
-        <div class="card">
+        <div class="card mb-3">
           <h5 class="card-header">{{ $note['title'] }}</h5>
           <div class="card-body">
             <p class="card-text">{{ $note['text'] }}</p>
